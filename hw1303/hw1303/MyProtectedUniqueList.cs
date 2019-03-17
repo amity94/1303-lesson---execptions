b7935c4cd6080437ce6f0f17e8003981dfa0796a
amity94 <45635119+amity94@users.noreply.github.com>
2019-03-17 11:44:15 +0200

    Add files via upload

---
+++ hw1303/hw1303/MyProtectedUniqueList.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1303
+{
+    class MyProtectedUniqueList : IEnumerable
+    {
+        private List<string> words;
+        public string Password;
+
+        public MyProtectedUniqueList(string password)
+        {
+            this.Password = password;
+            words = new List<string>(); 
+        }
+
+        public void Add(string word)
+        {
+            if (word == null && word == string.Empty)
+            {
+                throw new ArgumentNullException("word can't be empty");
+            }
+            if (words.Contains(word))
+            {
+                throw new InvalidOperationException("word already exists");
+            }
+            words.Add(word);
+        }
+
+        public void Remove(string word)
+        {
+            if (word == null && word == string.Empty)
+            {
+                throw new ArgumentNullException("word can't be empty");
+            }
+            if (!words.Contains(word))
+            {
+                throw new ArgumentException("word doesn't exist");
+            }
+            words.Remove(word);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if(index ==0 && index > words.Count)
+            {
+                throw new ArgumentOutOfRangeException("index is out of range");
+            }
+            words.RemoveAt(index);
+        }
+
+        public void Clear(string password)
+        {
+            if(password != Password)
+            {
+                throw new AccessViolationException("password incorrect");
+            }
+            words.Clear();
+        }
+
+        public void Sort(string password)
+        {
+            if(password != Password)
+            {
+                throw new AccessViolationException("password incorrect");
+            }
+            words.Sort();
+        }
+
+        public override string ToString()
+        {
+            foreach(var word in words)
+            return word;
+            return null;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return words.GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return words.GetEnumerator();
+        }
+
+        public string this [int index]
+        {
+            get
+            {
+                return words[index];
+            }
+        }
+    }
+}
---
+++ hw1303/hw1303/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProtectedUniqueList words = new MyProtectedUniqueList("hello");
+
+            words.Add("hello");
+            try
+            {
+                words.Add("hello");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                words.Add("");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            words.Add("world, ");
+            words.Add("how");
+            words.Add("are");
+            words.Add("you?");
+
+            foreach (var word in words)
+                Console.WriteLine(word);
+
+            words.Remove("are");
+            foreach (var word in words)
+                Console.WriteLine(word);
+            try
+            {
+                words.Remove("your");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            words.RemoveAt(2);
+            foreach (var word in words)
+                Console.WriteLine(word);
+
+            try
+            {
+                words.RemoveAt(5);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                words.Clear("pass");
+            }
+            catch (AccessViolationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //words.Clear("hello");
+            //foreach (var word in words)
+            //    Console.WriteLine(word);
+
+            words.Add("one");
+            words.Add("two");
+            words.Add("aaaaa");
+
+            try
+            {
+                words.Sort("hio");
+            }
+            catch(AccessViolationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            words.Sort("hello");
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
+    }
+}
